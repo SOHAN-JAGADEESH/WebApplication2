a02aeead877c267ea3e8e9fab5be1bdf518ce525
sohan <sohan@SOHAN>
2022-08-19 15:14:04 +1000

    Add project files.

---
+++ WebApplication2/Migrations/202208161035255_1InitialCreate.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _1InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Student", "LastName2", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Student", "LastName2");
+        }
+    }
+}
---
+++ WebApplication2/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2.Migrations
+{
+    using WebApplication2.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication2.DAL.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplication2.DAL.SchoolContext context)
+        {
+           
+        }
+    }
+}
